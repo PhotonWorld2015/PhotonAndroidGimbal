24c698f3d911cb2b01b97497b4278a835ae9749e
abinash sahoo <sabhinash88@gmail.com>
2015-04-10 22:05:56 +0530

    modified
    modified

--- src/android/com/android/photonworld/Beacon.java
+++ src/android/com/android/photonworld/Beacon.java
@@ -16,8 +16,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.location.LocationManager;
-import android.widget.Toast;
-import org.apache.cordova.CordovaPlugin;
+
 
 import com.gimbal.android.Gimbal;
 import com.gimbal.android.PlaceEventListener;
